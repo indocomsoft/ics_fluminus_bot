5d9e24a6309deda254e0e08e7b586432762ccb5f
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-11-27 15:39:51 +0800

    Prepare for dokku deployment

--- config/config.exs
+++ config/config.exs
@@ -9,4 +9,32 @@ config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 
 config :ics_fluminus_bot, name: :ics_fluminus_bot
 
-import_config "secrets.exs"
+token = System.get_env("TOKEN")
+
+case token do
+  nil ->
+    import_config "secrets.exs"
+
+  x when is_binary(x) ->
+    config :ex_gram, token: token
+
+    id =
+      System.get_env("ID") ||
+        raise """
+        environment variable ID is missing.
+        """
+
+    username =
+      System.get_env("USERNAME") ||
+        raise """
+        environment variable USERNAME is missing.
+        """
+
+    password =
+      System.get_env("PASSWORD") ||
+        raise """
+        environment variable PASSWORD is missing.
+        """
+
+    config :ics_fluminus_bot, id: id, credential: %{username: username, password: password}
+end
